e82f8d8fdaaa3704d9372084c19fe872634a78e1
Hera <mostafizhera@gmail.com>
2024-10-12 01:25:39 +0600

    Register successfull working completed

--- src/Register.jsx
+++ src/Register.jsx
@@ -1,32 +1,54 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-
+import axios from 'axios';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { API_URL } from './constants/constants';
+import AlertError from './Alert/AlertError';
+import AlertSucc from './Alert/AlertSucc';
 
 export default function Register() {
+
+    const [fullName, setFullName] = useState("");
+    const [email, setEmail] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+    const [password, setPassword] = useState("");
+    const [formStatus, setFormStatus] = useState({status: false, message:""});
+
+    const handleRegister = async()=>{
+        setIsLoading(true);
+        if(!fullName.length || !email.length || !password.length){
+            setFormStatus({status: false, message: "All fields are required"})
+        }else{
+            setTimeout( async () => {
+                const req = await axios.post(API_URL + '/register', {fullName,email,password});
+                console.log(req.data);
+        
+                if(req.data.status === true){
+                    setFormStatus({status: true, message: req.data.message});
+                }else{
+                    setFormStatus({status: false, message: req.data.message});
+                }
+                setIsLoading(false);
+            }, 500);
+        }
+        
+    }
+
   return (
     <>
          <div className=' h-screen flex justify-center items-center bg-slate-700'>
-        <form className=" mx-auto bg-slate-800 p-4 rounded-md w-1/2 shadow-lg shadow-gray-700">
+        <div className=" mx-auto bg-slate-800 p-4 rounded-md w-1/2 shadow-lg shadow-gray-700">
            <h2 className=' text-3xl font-bold text-center text-blue-700 py-6'>Register From</h2>
         <div className="mb-5">
             <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">FullName</label>
-            <input type="text" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter Your FullName" required />
-        </div>
-        <div className="mb-5">
-            <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">User Name</label>
-            <input type="text" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter your UserName" required />
+            <input onChange={e => setFullName(e.target.value)} value={fullName} type="text" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter Your FullName" required />
         </div>
         <div className="mb-5">
             <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Email</label>
-            <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter your Email" required />
+            <input onChange={e => setEmail(e.target.value)} value={email}  type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter your Email" required />
         </div>
         <div className="mb-5">
-            <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">New password</label>
-            <input type="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='Password' required />
-        </div>
-        <div className="mb-5">
-            <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confrom password</label>
-            <input type="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='Confrom Password' required />
+            <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">password</label>
+            <input onChange={e => setPassword(e.target.value)} value={password}  type="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='Password' required />
         </div>
         <div className=" flex justify-between items-center mb-5">
             <div className='flex items-start'>
@@ -35,10 +57,12 @@ export default function Register() {
             </div>
             <label htmlFor="remember" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label>
             </div>
-            <div><Link to="/login" className=" text-blue-700 hover:underline">Login here</Link></div>
+            <div><span className=' text-white mr-1'>Already hava an account</span> <Link to="/login" className=" text-blue-700 hover:underline">Login here</Link></div>
         </div>
-        <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Register Now</button>
-        </form>
+        {formStatus?.status===false && formStatus?.message?.length? (<AlertError message={formStatus?.message}/>) : null}
+        {formStatus?.status===true && formStatus?.message?.length? (<AlertSucc message={formStatus?.message}/>) : null}
+        <button onClick={handleRegister} type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">{isLoading == true ? "Please wait..." : "Register Now"}</button>
+        </div>
      </div> 
     </>
   )
